03acc5b0b655c49612566b82f2773c8672a77fd7
Denis Ruparel <deniskalpeshbhai436@gmail.com>
2025-01-24 18:58:48 +0530

    image functionalities are completed

--- components/toolbar/image-tools.tsx
+++ components/toolbar/image-tools.tsx
@@ -1,20 +1,20 @@
 "use client"
 
-// import AIRecolor from "@/components/toolbar/recolor"
-// import GenerativeFill from "@/components/toolbar/generative-fill"
+import AIRecolor from "@/components/toolbar/recolor"
+import GenerativeFill from "@/components/toolbar/generative-fill"
 import GenRemove from "@/components/toolbar/gen-remove"
 import BgRemove from "./bg-remove"
 import AIBackgroundReplace from "./bg-replace"
-// import ExtractPart from "./extract-part"
+import ExtractPart from "./extract-part"
 
 export default function ImageTools() {
   return (
     <>
-      {/* <GenerativeFill /> */}
-      {/* <AIRecolor /> */}
+      <GenerativeFill />
+      <AIRecolor />
       <GenRemove />
       <AIBackgroundReplace />
-      {/* <ExtractPart /> */}
+      <ExtractPart />
       <BgRemove />
     </>
   )
